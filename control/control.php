281ca5b30095566a9f95a087b94369ea662b5d7d
Andrew Quaye <andyquayegh@outlook.com>
2022-09-28 11:59:39 +0100

    update

--- control/control.php
+++ control/control.php
@@ -1,5 +1,20 @@
 <?php
 
+// inactive in seconds
+$inactive = 10 * 60;
+if( !isset($_SESSION['timeout']) )
+
+$_SESSION['timeout'] = time() + $inactive; 
+
+$session_life = time() - $_SESSION['timeout'];
+
+if($session_life > $inactive){  
+    session_destroy(); 
+    header("Location:index.php");     
+}
+
+$_SESSION['timeout']=time();
+
 function config($str){
 
     if(!file_exists("config.json")){
