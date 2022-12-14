c88ce2f6eef7519437b5572b4f04df29923a9f29
Andrew Quaye <andyquayegh@outlook.com>
2022-12-14 09:02:28 +0100

    change Icon
    change Icon

--- client/menu.php
+++ client/menu.php
@@ -1,31 +1,31 @@
 <ul class="metismenu" id="menu">
     <li><a href="?client=dashboard" class="ai-icon" aria-expanded="false">
-            <i class="flaticon-layout"></i>
+            <i class="flaticon-dashboard"></i>
             <span class="nav-text">Dashboard</span>
         </a>
     </li>
     <li><a href="?client=topup" class="ai-icon" aria-expanded="false">
-            <i class="flaticon-admin"></i>
+            <i class="flaticon-wallet"></i>
             <span class="nav-text">TopUp</span>
         </a>
     </li>
     <li><a href="javascript:void(0)"  data-bs-toggle="modal" data-bs-target="#QuickSMS" class="ai-icon" aria-expanded="false">
-            <i class="flaticon-admin"></i>
+            <i class="flaticon-message"></i>
             <span class="nav-text">Quick SMS</span>
         </a>
     </li>
     <li><a href="javascript:void(0)"  data-bs-toggle="modal" data-bs-target="#BulkSMS"  class="ai-icon" aria-expanded="false">
-            <i class="flaticon-admin"></i>
+            <i class="flaticon-message"></i>
             <span class="nav-text">Bulk SMS</span>
         </a>
     </li>
     <li><a href="?client=schedule" class="ai-icon" aria-expanded="false">
-            <i class="flaticon-admin"></i>
+            <i class="flaticon-register"></i>
             <span class="nav-text">SMS Schedule</span>
         </a>
     </li>
     <li><a href="?client=group" class="ai-icon" aria-expanded="false">
-            <i class="flaticon-admin"></i>
+            <i class="flaticon-smartphone"></i>
             <span class="nav-text">Contact</span>
         </a>
     </li>
@@ -35,7 +35,7 @@
         </a>
     </li>
     <li><a href="?client=sms-log" class="ai-icon" aria-expanded="false">
-            <i class="flaticon-admin"></i>
+            <i class="flaticon-newsletter"></i>
             <span class="nav-text">SMS Logs</span>
         </a>
     </li>
